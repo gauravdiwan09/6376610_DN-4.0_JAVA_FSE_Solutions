575d05033358ad04d58d3dbac841538fc079be6d
gauravdiwan09 <gauravdiwan10th@gmail.com>
2025-07-25 16:33:01 +0530

    Week6_ReactJS

---
+++ JAVA_FSE_HANDS_ON_Solutions/Week 6/Week6_ReactJS/2_ReactJS-HOL/Code/src/Components/Home.js
@@ -0,0 +1,13 @@
+import { Component } from "react";
+
+class Home extends Component {
+  render() {
+    return (
+      <div>
+        <h3 align="center">Welcome to the Home page of Student Management Portal</h3>
+      </div>
+    );
+  }
+}
+
+export default Home;
